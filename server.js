cfb52baf8113e7575f713f88a8e425c16a59b555
Brittany Storoz <brittanystoroz@gmail.com>
2017-08-21 10:19:45 -0600

    Add console.logs

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+
+console.log('WHATEVER');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
